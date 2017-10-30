562feb5b31ee12b454d38db63a6c61e48b2c4683
Hoang Nguyen <hoangnt.icloud@gmail.com>
2017-10-30 23:39:06 +0700

    up

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :rails_env, :production
 
-server '52.221.194.248', user: 'ubuntu', roles: %w{web app db}, primary: true
+server '163.44.206.110', user: 'root', roles: %w{web app db}, primary: true
