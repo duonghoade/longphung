6b2a817fdb6a948549f876d4c10bfff87dda9cad
Hoang Nguyen <hoangnt.icloud@gmail.com>
2017-11-13 16:11:59 +0700

    111

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # http_basic_authenticate_with name: "aoehd", password: "aoehd"
+  http_basic_authenticate_with name: "111", password: "111"
 end
