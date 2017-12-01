9bc16fa0b8bf3d9e23f61cb7502fcfb539f3efb7
Hoang Nguyen <hoangnt.icloud@gmail.com>
2017-12-01 17:48:20 +0700

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :basic
+  # before_action :basic
 
   def basic
     authenticate_or_request_with_http_basic do |user, pass|
